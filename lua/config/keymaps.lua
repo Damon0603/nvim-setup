eb9fab3fce7953c9fcdec7be3dfd7a1d2a200dd2
Sebastian Breuer <sebastian.breuer@insta-drive.com>
2024-09-11 11:40:50 +0200

    Custom run config for lua and python files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,16 @@
 --
 
 -- Execute Python Commands in Terminal
-vim.api.nvim_set_keymap("n", "<leader>r", ":w<CR>:term python3 %<CR>i", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<leader>r", ":w<CR>:term python3 %<CR>i", { noremap = true, silent = true })
+
+-- Execute Python or Lua using leader r and the respective p or l
+
+-- Map Leader + r + p for running Python files
+vim.api.nvim_set_keymap("n", "<leader>rp", ":w<CR>:term python3 %<CR>i", { noremap = true, silent = true })
+
+-- Map Leader + r + l for running Lua files
+vim.api.nvim_set_keymap("n", "<leader>rl", ":w<CR>:term lua %<CR>i", { noremap = true, silent = true })
+
 --
 -- Close current buffer using Leader + ö
 vim.api.nvim_set_keymap("n", "<Leader>ö", ":tabclose<CR>", { noremap = true, silent = true })
